217724c879691a8a485fb64fc401b730f615d79c
Matt Mihic <mmihic@uber.com>
2016-08-23 13:38:16 -0400

    fill in placement logic

--- placement/placement.go
+++ placement/placement.go
@@ -42,13 +42,13 @@ var (
 type CommitOptions interface {
 	// RolloutDelay is the amount of time to wait for the configuration to be
 	// distributed across all interested listeners
-	RolloutDelay() time.Duration
-	SetRolloutDelay(t time.Duration) CommitOptions
+	GetRolloutDelay() time.Duration
+	RolloutDelay(t time.Duration) CommitOptions
 
 	// TransitionDelay is the amount of time to wait for the cluster to converge
 	// on the same state after a transition
-	TransitionDelay() time.Duration
-	SetTransitionDelay(t time.Duration) CommitOptions
+	GetTransitionDelay() time.Duration
+	TransitionDelay(t time.Duration) CommitOptions
 }
 
 // StoragePlacement handles mapping shards
@@ -175,6 +175,7 @@ func (sp storagePlacement) JoinCluster(dbName string, c schema.ClusterProperties
 			Cluster: &schema.Cluster{
 				Name:      c.Name,
 				Type:      c.Type,
+				Weight:    c.Weight,
 				Status:    schema.ClusterStatus_ACTIVE,
 				CreatedAt: xtime.ToUnixMillis(sp.clock.Now()),
 			},
@@ -218,12 +219,212 @@ func (sp storagePlacement) GetPendingChanges() (int, *schema.Placement, *schema.
 
 func (sp storagePlacement) CommitChanges(version int, opts CommitOptions) error {
 	return sp.mgr.Commit(version, wrapFn(func(p *schema.Placement, changes *schema.PlacementChanges) error {
+		for name, add := range changes.DatabaseAdds {
+			change := changes.DatabaseChanges[name]
+			if err := sp.commitNewDatabase(p, add.Database, change, opts); err != nil {
+				return err
+			}
+
+			// Don't double process as a set of changes
+			delete(changes.DatabaseChanges, name)
+		}
+
+		for name, change := range changes.DatabaseChanges {
+			if err := sp.commitDatabaseChanges(p, p.Databases[name], change, opts); err != nil {
+				return err
+			}
+		}
+
 		return nil
 	}))
 }
 
-func (sp storagePlacement) findDatabase(p *schema.Placement, changes *schema.PlacementChanges, name string,
-) (*schema.Database, *schema.DatabaseChanges) {
+func (sp storagePlacement) commitNewDatabase(
+	p *schema.Placement,
+	db *schema.Database,
+	changes *schema.DatabaseChanges,
+	opts CommitOptions) error {
+	// Check to see if there are any existing databases that cover this retention
+	// period - if so then we need to do a staged cutover to that database
+	// TODO(mmihic): This will do a staged cutover if two newly introduced databases
+	// overlap with each other
+	needsStagedCutover := false
+	for _, existing := range p.Databases {
+		needsStagedCutover = needsStagedCutover || existing.MaxRetentionInSecs > db.MaxRetentionInSecs
+	}
+
+	if needsStagedCutover {
+		var (
+			readCutoverTime     = sp.clock.Now().Add(opts.GetRolloutDelay())
+			writeCutoverTime    = readCutoverTime.Add(opts.GetRolloutDelay())
+			cutoverCompleteTime = writeCutoverTime.Add(opts.GetTransitionDelay())
+		)
+
+		db.ReadCutoverTime = xtime.ToUnixMillis(readCutoverTime)
+		db.WriteCutoverTime = xtime.ToUnixMillis(writeCutoverTime)
+		db.CutoverCompleteTime = xtime.ToUnixMillis(cutoverCompleteTime)
+	}
+
+	p.Databases[db.Name] = db
+	return sp.commitDatabaseChanges(p, db, changes, opts)
+}
+
+func (sp storagePlacement) commitDatabaseChanges(
+	p *schema.Placement,
+	db *schema.Database,
+	changes *schema.DatabaseChanges,
+	opts CommitOptions) error {
+
+	var (
+		unowned       []uint32
+		shardCutoffs  = make(map[string][]uint32)
+		shardCutovers = make(map[string][]uint32)
+	)
+
+	// Add all joining cluster to the set of active clusters
+	for name, join := range changes.Joins {
+		db.Clusters[name] = join.Cluster
+		db.ShardAssignments[name] = &schema.ClusterShardAssignment{}
+	}
+
+	// Go through all decomissioned clusters, remove them from the active
+	// clusters list, and add their shards to an "unowned" pool
+	for name := range changes.Decomms {
+		delete(db.Clusters, name)
+
+		shards := db.ShardAssignments[name]
+		if shards == nil {
+			continue
+		}
+
+		unowned = append(unowned, shards.Shards...)
+		shardCutoffs[name] = append(shardCutoffs[name], shards.Shards...)
+		delete(db.ShardAssignments, name)
+	}
+
+	// Rebalance shards on the active clusters.
+	// pass 1, return shards on overweight clusters to the "unowned" pool
+	desiredNumShards := sp.computeDesiredNumShards(db.Clusters, int(db.NumShards))
+	for name := range db.Clusters {
+		var (
+			desired  = desiredNumShards[name]
+			assigned = db.ShardAssignments[name]
+			removed  []uint32
+		)
+
+		if desired >= len(assigned.Shards) {
+			continue // Will acquire more shards in pass 2
+		}
+
+		numToRemove := len(assigned.Shards) - desired
+		removed, assigned.Shards = assigned.Shards[:numToRemove], assigned.Shards[numToRemove+1:]
+		unowned = append(unowned, removed...)
+		shardCutoffs[name] = append(shardCutoffs[name], removed...)
+	}
+
+	// pass 2, assign shards from the "unowned" pool to underweight clusters
+	for name := range db.Clusters {
+		var (
+			desired  = desiredNumShards[name]
+			assigned = db.ShardAssignments[name]
+			added    []uint32
+		)
+
+		if desired <= len(assigned.Shards) {
+			continue // NB(mmihic): Should never be less at this point
+		}
+
+		// TODO(mmihic): Maybe validate that the numToAdd is >= the number remaining
+		numToAdd := desired - len(assigned.Shards)
+		added, unowned = unowned[:numToAdd], unowned[numToAdd:]
+		shardCutovers[name] = append(shardCutovers[name], added...)
+		assigned.Shards = append(assigned.Shards, added...)
+	}
+
+	// now build out all of the rules
+	db.Version++
+	rules := &schema.ClusterMappingRuleSet{
+		ForVersion: db.Version,
+	}
+
+	var (
+		readCutoverTime  = sp.clock.Now()
+		writeCutoverTime = readCutoverTime.Add(opts.GetRolloutDelay())
+		cutoffTime       = writeCutoverTime.Add(opts.GetTransitionDelay())
+	)
+
+	for name, shards := range shardCutovers {
+		// TODO(mmihic): If this is a new database we don't need graceful cutovers
+		rules.Cutovers = append(rules.Cutovers, &schema.CutoverRule{
+			ClusterName:      name,
+			Shards:           shards,
+			ReadCutoverTime:  xtime.ToUnixMillis(readCutoverTime),
+			WriteCutoverTime: xtime.ToUnixMillis(writeCutoverTime),
+		})
+	}
+
+	for name, shards := range shardCutoffs {
+		rules.Cutoffs = append(rules.Cutoffs, &schema.CutoffRule{
+			ClusterName: name,
+			Shards:      shards,
+			CutoffTime:  xtime.ToUnixMillis(cutoffTime),
+		})
+	}
+
+	db.MappingRules = append(db.MappingRules, rules)
+	return nil
+}
+
+// computeDesiredNumShards computes the desired number of shards per cluster,
+// based on the cluster weight and total number of shards
+func (sp storagePlacement) computeDesiredNumShards(
+	clusters map[string]*schema.Cluster,
+	totalShards int) map[string]int {
+
+	if len(clusters) == 0 {
+		return nil
+	}
+
+	// Compute the total weight of all clusters
+	var totalWeight uint32
+	for _, c := range clusters {
+		totalWeight += c.Weight
+	}
+
+	// compute desired # of shards based on relative weight of each cluster
+	var (
+		oldestCluster    *schema.Cluster
+		shardsRemaining  = totalShards
+		desiredNumShards = make(map[string]int, len(clusters))
+	)
+	for name, c := range clusters {
+		relativeWeight := 100 * (float64(c.Weight) / float64(totalWeight))
+		numShards := int(relativeWeight * float64(totalShards))
+		if numShards > shardsRemaining {
+			numShards = shardsRemaining
+		}
+
+		desiredNumShards[name] = numShards
+		shardsRemaining -= numShards
+
+		if oldestCluster == nil || oldestCluster.CreatedAt > c.CreatedAt {
+			oldestCluster = c
+		}
+	}
+
+	// if we haven't yet allocated all shards due to rounding, pick oldest
+	// cluster and add the remainder to it. we pick the oldest cluster to have a
+	// deterministic mapping and avoid moving shards around unnecessarily
+	if shardsRemaining > 0 {
+		desiredNumShards[oldestCluster.Name] += shardsRemaining
+	}
+	return desiredNumShards
+}
+
+func (sp storagePlacement) findDatabase(
+	p *schema.Placement,
+	changes *schema.PlacementChanges,
+	name string) (*schema.Database, *schema.DatabaseChanges) {
 
 	db := p.Databases[name]
 	if db == nil {
