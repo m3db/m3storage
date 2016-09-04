21ff89424a79f2a17c8bcf4121f9ba36fdc857d4
Matt Mihic <mmihic@uber.com>
2016-09-04 18:49:24 -0400

    move retention related stuff into retention pkg

--- retention/retention_rule_test.go
+++ retention/retention_rule_test.go
@@ -16,7 +16,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package storage
+package retention
 
 import (
 	"sort"
@@ -31,50 +31,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRetentionRulesByCutoffTime(t *testing.T) {
+func TestRulesByCutoffTime(t *testing.T) {
 	now := time.Now()
 
-	rules := []RetentionRule{
-		NewRetentionRule().SetCutoffTime(now.Add(-time.Hour * 12)),
-		NewRetentionRule(),
-		NewRetentionRule().SetCutoffTime(now.Add(-time.Hour * 6)),
+	rules := []Rule{
+		NewRule().SetCutoffTime(now.Add(-time.Hour * 12)),
+		NewRule(),
+		NewRule().SetCutoffTime(now.Add(-time.Hour * 6)),
 	}
 
-	sort.Sort(RetentionRulesByCutoffTime(rules))
+	sort.Sort(RulesByCutoffTime(rules))
 	assert.True(t, rules[0].CutoffTime().IsZero())
 	assert.Equal(t, time.Hour*6, now.Sub(rules[1].CutoffTime()))
 	assert.Equal(t, time.Hour*12, now.Sub(rules[2].CutoffTime()))
 }
 
-func TestParseRetentionPolicy(t *testing.T) {
-	p, err := ParseRetentionPolicy("10s:2d")
+func TestParsePolicy(t *testing.T) {
+	p, err := ParsePolicy("10s:2d")
 	require.NoError(t, err)
 	assert.Equal(t, (time.Second * 10).String(), p.Resolution().WindowSize().String())
 	assert.Equal(t, xtime.Millisecond, p.Resolution().Precision())
-	assert.Equal(t, (time.Hour * 24 * 2).String(), p.RetentionPeriod().Duration().String())
+	assert.Equal(t, (time.Hour * 24 * 2).String(), p.Period().Duration().String())
 
-	p, err = ParseRetentionPolicy("1min@1s:30d")
+	p, err = ParsePolicy("1min@1s:30d")
 	require.NoError(t, err)
 	assert.Equal(t, (time.Minute).String(), p.Resolution().WindowSize().String())
 	assert.Equal(t, xtime.Second, p.Resolution().Precision())
-	assert.Equal(t, (time.Hour * 24 * 30).String(), p.RetentionPeriod().Duration().String())
+	assert.Equal(t, (time.Hour * 24 * 30).String(), p.Period().Duration().String())
 
-	p, err = ParseRetentionPolicy("10monkeys:2d")
+	p, err = ParsePolicy("10monkeys:2d")
 	require.Error(t, err)
 	assert.Nil(t, p)
 
-	p, err = ParseRetentionPolicy("10s:10monkeys")
+	p, err = ParsePolicy("10s:10monkeys")
 	require.Error(t, err)
 	assert.Nil(t, p)
 
-	p, err = ParseRetentionPolicy("10s")
+	p, err = ParsePolicy("10s")
 	require.Error(t, err)
 	assert.Nil(t, p)
 }
 
-func TestParseRetentionPolicies(t *testing.T) {
+func TestParsePolicies(t *testing.T) {
 	// NB(mmihic): These are out of order so will need to be sorted
-	policies, err := ParseRetentionPolicies("5s:2d,10s:2d,1min:7d,500ms:6h,5min:14d")
+	policies, err := ParsePolicies("5s:2d,10s:2d,1min:7d,500ms:6h,5min:14d")
 	require.NoError(t, err)
 
 	for n, expected := range []struct {
@@ -88,7 +88,7 @@ func TestParseRetentionPolicies(t *testing.T) {
 		{time.Minute * 5, time.Hour * 24 * 14},
 	} {
 		require.Equal(t, expected.resolution.String(), policies[n].Resolution().WindowSize().String())
-		require.Equal(t, expected.retention.String(), policies[n].RetentionPeriod().Duration().String())
+		require.Equal(t, expected.retention.String(), policies[n].Period().Duration().String())
 	}
 }
 
@@ -106,14 +106,14 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 	tests := []struct {
 		name        string
 		from, until time.Duration
-		rules       []rule
+		rules       []ruleSpec
 		results     []expectedQuery
 	}{
 		// query whose end time is past the cutoff for the rule - should be
 		// truncated to cutoff point
 		{name: "query extends after end",
 			from: day * 60, until: day * 1,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:2d,1min:45d", cutoff: day * 5},
 			},
 			results: []expectedQuery{
@@ -125,7 +125,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// be truncated to cutover point
 		{name: "query extends before start",
 			from: day * 60, until: day * 1,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:2d,1min:45d", cutover: day * 30},
 			},
 			results: []expectedQuery{
@@ -137,7 +137,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// rule with cutoff time, query entirely past cutoff time
 		{name: "query entirely outside rules",
 			from: day * 5, until: 0,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:2d", cutoff: day * 30},
 			},
 			results: []expectedQuery{},
@@ -146,7 +146,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// rule with cutover and cutoff time, but query entirely within rule bounds
 		{name: "query entirely within rules",
 			from: day * 5, until: day * 1,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:7d,1min:30d", cutoff: time.Hour * 2, cutover: day * 40},
 			},
 			results: []expectedQuery{
@@ -157,7 +157,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// rule without cutover or cutoff time, query fully satisfied
 		{name: "rules extend forever",
 			from: day * 10, until: 0,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:2d,1min:45d"},
 			},
 			results: []expectedQuery{
@@ -170,7 +170,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// simply cutover between two rules
 		{name: "simple cutover",
 			from: day * 19, until: 0,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "1s:12h,10s:2d,1min:7d,5min:14d", cutover: time.Hour * 6},
 				{policies: "10s:12h,1min:3d,5min:30d", cutoff: time.Hour * 6},
 			},
@@ -185,7 +185,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// multiple cutovers between rules
 		{name: "multiple cutovers",
 			from: day * 19, until: 0,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "10s:1d,1min:5d,5min:30d", cutover: time.Hour * 14},
 				{policies: "1s:12h,10s:1d,1min:5d,5min:30d", cutoff: time.Hour * 14, cutover: day * 3},
 				{policies: "10s:2d,1min:7d,5min:30d", cutoff: day * 3, cutover: day * 6},
@@ -202,7 +202,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 		// duplicate retention period
 		{name: "duplicate retention period",
 			from: day * 19, until: 0,
-			rules: []rule{
+			rules: []ruleSpec{
 				{policies: "10s:1d,1s:1d,1min:5d,5min:30d"},
 			},
 			results: []expectedQuery{
@@ -227,7 +227,7 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 			}
 
 			expected, actual := expectedPlan[i], actualPlan[i]
-			require.Equal(t, expected.RetentionPolicy.String(), actual.RetentionPolicy.String(),
+			require.Equal(t, expected.Policy.String(), actual.Policy.String(),
 				"invalid retention policy for %s[%d]", test.name, i)
 			require.Equal(t, now.Sub(expected.Start).String(), now.Sub(actual.Start).String(),
 				"invalid from for %s[%d]", test.name, i)
@@ -237,11 +237,11 @@ func TestBuildRetentionQueryPlan(t *testing.T) {
 	}
 }
 
-func buildRules(t *testing.T, now time.Time, rulesSpec []rule) []RetentionRule {
-	rules := make([]RetentionRule, 0, len(rulesSpec))
+func buildRules(t *testing.T, now time.Time, rulesSpec []ruleSpec) []Rule {
+	rules := make([]Rule, 0, len(rulesSpec))
 	for _, spec := range rulesSpec {
-		policies := MustParseRetentionPolicies(spec.policies)
-		rule := NewRetentionRule().SetRetentionPolicies(policies)
+		policies := MustParsePolicies(spec.policies)
+		rule := NewRule().SetPolicies(policies)
 		if spec.cutoff > 0 {
 			rule.SetCutoffTime(now.Add(-spec.cutoff))
 		}
@@ -251,7 +251,7 @@ func buildRules(t *testing.T, now time.Time, rulesSpec []rule) []RetentionRule {
 		}
 		rules = append(rules, rule)
 	}
-	sort.Sort(RetentionRulesByCutoffTime(rules))
+	sort.Sort(RulesByCutoffTime(rules))
 	return rules
 }
 
@@ -263,14 +263,14 @@ func buildExpectedQueries(t *testing.T, now time.Time, expected []expectedQuery)
 				Start: now.Add(-q.from),
 				End:   now.Add(-q.until),
 			},
-			RetentionPolicy: MustParseRetentionPolicy(q.policy),
+			Policy: MustParsePolicy(q.policy),
 		})
 	}
 
 	return queries
 }
 
-type rule struct {
+type ruleSpec struct {
 	cutover, cutoff time.Duration
 	policies        string
 }
